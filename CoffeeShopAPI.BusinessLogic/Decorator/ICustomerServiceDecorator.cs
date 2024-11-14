1c44cf6e83b75c3d768fcbff42be699fd5265dfa
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2024-11-15 01:58:20 +0200

    added decorator
    also:
      updated --> DependencyInjection
      added --> Logging, Scrutor

---
+++ CoffeeShopAPI.BusinessLogic/Decorator/ICustomerServiceDecorator.cs
@@ -0,0 +1,8 @@
+﻿using CoffeeShopAPI.BusinessLogic.Contracts;
+
+namespace CoffeeShopAPI.BusinessLogic.Decorators
+{
+    public interface ICustomerServiceDecorator : ICustomersService
+    {
+    }
+}
